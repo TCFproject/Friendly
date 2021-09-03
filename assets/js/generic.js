baa57b4862a0b52403e7b6bcd2525cb7e701f694
TCFproject <thierry.changfong@epsi.fr>
2021-09-03 11:19:40 +0200

    Changement de lien de generis.js

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -2,5 +2,5 @@ var listChapitre = document.getElementById('selectChapitre');
 var chemin = document.getElementById('route');
 
 listChapitre.addEventListener('change', function () {
-    window.location.href = 'http://localhost/messites/local/testTemplate/generic.php?chemin='+chemin.value+'&chapitre='+listChapitre.value;
+    window.location.href = 'http://localhost/messites/Friendly/generic.php?chemin='+chemin.value+'&chapitre='+listChapitre.value;
 });
